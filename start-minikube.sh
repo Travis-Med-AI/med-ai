230616bd276a2120ce059acfc19f213819f5d17b
Travis Clarke <travisjonathanclarke@gmail.com>
2022-03-31 22:10:54 -0400

    updated

--- start-minikube.sh
+++ start-minikube.sh
@@ -1,4 +1,4 @@
-minikube start --driver=docker --extra-config=apiserver.service-node-port-range=1-65535 --cpus 16 memory 16000
+minikube start --driver=docker --extra-config=apiserver.service-node-port-range=1-65535 --cpus 3 --memory 16000
 
 kubectl create clusterrolebinding serviceaccounts-cluster-admin \
   --clusterrole=cluster-admin \
